6d7a4ac624ebb8f1f416108be71fa96290f09c4c
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-09 22:51:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,6 @@ def create_project_hash
 
   projects = {}
 
-  #Iterate through the projects
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
